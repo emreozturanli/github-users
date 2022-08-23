55ae023da313c67591dbc9f77b935998a7c3be02
emre <emreozturanli1@gmail.com>
2022-08-23 12:31:47 +0300

    bar chart added

--- src/components/Charts/Bar.tsx
+++ src/components/Charts/Bar.tsx
@@ -3,8 +3,8 @@ import { IData } from "./Charts"
 
 import ReactFC from "react-fusioncharts";
 import FusionCharts from "fusioncharts";
-import PieChart from "fusioncharts/fusioncharts.charts";
-ReactFC.fcRoot(FusionCharts, PieChart);
+import BarChart from "fusioncharts/fusioncharts.charts";
+ReactFC.fcRoot(FusionCharts, BarChart);
 
 
 interface IPieProps {
@@ -15,13 +15,13 @@ const Pie:FC<IPieProps> = (props) => {
   const {data} = props
 
   const chartConfigs = {
-    type: "pie3d", 
+    type: "bar3d", 
     width: "100%", 
     height: "300", 
     dataFormat: "json", 
     dataSource: {
       chart: {
-        caption: "Most Used Languages",
+        caption: "Most Stars",
         captionFontColor: "#102a42",
         captionFontSize: 20,
         baseFontSize: 16,
@@ -30,9 +30,7 @@ const Pie:FC<IPieProps> = (props) => {
         paletteColors:
           "#2caeba, #5D62B5, #FFC533, #F2726F, #8d6e63, #1de9b6, #6E80CA",
         use3DLighting: 1,
-        useDataPlotColorForLabels: 0,
         decimals: 0,
-        pieRadius: "40%",
         animation: 1,
         pieYScale: 70,
         showHoverEffect : 1,
--- src/components/Charts/Charts.tsx
+++ src/components/Charts/Charts.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import { useAppSelector } from '../../app/hooks'
 import { IRepos } from '../../types/interfaces'
 import Pie from './Pie'
+import Bar from './Bar'
 
  export interface IData{
   label: string
@@ -22,7 +23,7 @@ const Charts = () => {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => { getRepos() }, [user])
 
-  // pie chart data
+  // pie chart data (languages)
   const languages = repos?.reduce((total: any, repo) => {
     const lang = repo.language
     if (lang !== null) {
@@ -39,15 +40,28 @@ const Charts = () => {
     return total
   }, {})
 
-
   const pieData : IData[] | any = Object.values(languages).sort((a: any, b: any) => {
     return b.value - a.value
   }).slice(0, 5)
+
+  // bar chart data (stars)
+const stars = repos?.reduce((total: any, repo)=>{
+  total[repo.name]={label: repo.name, value: repo.stargazers_count}
+  return total
+},{})
+
+const barData : IData[] | any = Object.values(stars).sort((a : any, b: any)=>{
+   return b.value - a.value
+  }).slice(0, 5)
+
+  console.log(barData)
+
 
 
   return (
     <section className="max-w-3xl m-auto  px-4 mb-6 ">
       <Pie data={pieData}/>
+      <Bar data={barData}/>
     </section>
   )
 }
